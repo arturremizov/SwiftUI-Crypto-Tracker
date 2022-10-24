d4d32b47c667f8badfab8dfd64b4ea5d9621c148
Artur <arturremizov@gmail.com>
2022-10-24 14:31:35 +0300

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Artur Remizov on 24.10.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
