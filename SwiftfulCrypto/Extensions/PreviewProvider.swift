d288d3e844f98afe44d6068dfe4e7c96e3cda4f7
Artur <arturremizov@gmail.com>
2022-10-25 17:37:24 +0300

    design a row to display coins in a List

---
+++ SwiftfulCrypto/Extensions/PreviewProvider.swift
@@ -0,0 +1,35 @@
+//
+//  PreviewProvider.swift
+//  SwiftfulCrypto
+//
+//  Created by Artur Remizov on 25.10.22.
+//
+
+import Foundation
+import SwiftUI
+
+extension PreviewProvider {
+    
+    static var dev: DeveloperPreview {
+        return DeveloperPreview.instance
+    }
+}
+
+class DeveloperPreview {
+    
+    static let instance = DeveloperPreview()
+    private init() { }
+    
+    let coins: [CoinModel] = loadMockCoins()
+}
+
+private func loadMockCoins() -> [CoinModel] {
+    guard
+        let url = Bundle.main.url(forResource: "CoinModelsMock", withExtension: "json"),
+        let data = try? Data(contentsOf: url) else { return [] }
+    
+    let decoder = JSONDecoder()
+    decoder.keyDecodingStrategy = .convertFromSnakeCase
+    let coins = try? decoder.decode([CoinModel].self, from: data)
+    return coins ?? []
+}
